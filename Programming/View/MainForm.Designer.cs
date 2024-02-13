9b71f0770ff3ae8ad98ec867c49fc6dc7eb43574
Kirill Aksenov <kiraaxenow@gmail.com>
2024-02-13 14:08:13 +0700

    Сделал все виджеты

--- Programming/View/MainForm.Designer.cs
+++ Programming/View/MainForm.Designer.cs
@@ -31,10 +31,12 @@
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
             groupBox1 = new GroupBox();
-            EnumsListBox = new ListBox();
-            ValuesListBox = new ListBox();
-            EnumLabel = new Label();
+            ValueLabel = new Label();
+            ValuesTextBox = new TextBox();
             ValuesLabel = new Label();
+            EnumLabel = new Label();
+            ValuesListBox = new ListBox();
+            EnumsListBox = new ListBox();
             tabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
             groupBox1.SuspendLayout();
@@ -47,7 +49,7 @@
             tabControl1.Location = new Point(0, 0);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(791, 450);
+            tabControl1.Size = new Size(720, 395);
             tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -56,13 +58,15 @@
             tabPage1.Location = new Point(4, 29);
             tabPage1.Name = "tabPage1";
             tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(783, 417);
+            tabPage1.Size = new Size(712, 362);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "Enums";
             tabPage1.UseVisualStyleBackColor = true;
             // 
             // groupBox1
             // 
+            groupBox1.Controls.Add(ValueLabel);
+            groupBox1.Controls.Add(ValuesTextBox);
             groupBox1.Controls.Add(ValuesLabel);
             groupBox1.Controls.Add(EnumLabel);
             groupBox1.Controls.Add(ValuesListBox);
@@ -70,19 +74,44 @@
             groupBox1.Dock = DockStyle.Fill;
             groupBox1.Location = new Point(3, 3);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(777, 411);
+            groupBox1.Size = new Size(706, 356);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "Enumerations";
             // 
-            // EnumsListBox
+            // ValueLabel
             // 
-            EnumsListBox.FormattingEnabled = true;
-            EnumsListBox.ItemHeight = 20;
-            EnumsListBox.Location = new Point(47, 76);
-            EnumsListBox.Name = "EnumsListBox";
-            EnumsListBox.Size = new Size(150, 104);
-            EnumsListBox.TabIndex = 0;
+            ValueLabel.AutoSize = true;
+            ValueLabel.Location = new Point(478, 41);
+            ValueLabel.Name = "ValueLabel";
+            ValueLabel.Size = new Size(68, 20);
+            ValueLabel.TabIndex = 5;
+            ValueLabel.Text = "Int value;";
+            // 
+            // ValuesTextBox
+            // 
+            ValuesTextBox.Location = new Point(474, 76);
+            ValuesTextBox.Name = "ValuesTextBox";
+            ValuesTextBox.Size = new Size(125, 27);
+            ValuesTextBox.TabIndex = 4;
+            // 
+            // ValuesLabel
+            // 
+            ValuesLabel.AutoSize = true;
+            ValuesLabel.Location = new Point(254, 48);
+            ValuesLabel.Name = "ValuesLabel";
+            ValuesLabel.Size = new Size(97, 20);
+            ValuesLabel.TabIndex = 3;
+            ValuesLabel.Text = "Choose value";
+            // 
+            // EnumLabel
+            // 
+            EnumLabel.AutoSize = true;
+            EnumLabel.Location = new Point(47, 48);
+            EnumLabel.Name = "EnumLabel";
+            EnumLabel.Size = new Size(142, 20);
+            EnumLabel.TabIndex = 2;
+            EnumLabel.Text = "Choose enumiration";
             // 
             // ValuesListBox
             // 
@@ -93,29 +122,20 @@
             ValuesListBox.Size = new Size(150, 104);
             ValuesListBox.TabIndex = 1;
             // 
-            // EnumLabel
+            // EnumsListBox
             // 
-            EnumLabel.AutoSize = true;
-            EnumLabel.Location = new Point(47, 48);
-            EnumLabel.Name = "EnumLabel";
-            EnumLabel.Size = new Size(50, 20);
-            EnumLabel.TabIndex = 2;
-            EnumLabel.Text = "label1";
-            // 
-            // ValuesLabel
-            // 
-            ValuesLabel.AutoSize = true;
-            ValuesLabel.Location = new Point(254, 48);
-            ValuesLabel.Name = "ValuesLabel";
-            ValuesLabel.Size = new Size(50, 20);
-            ValuesLabel.TabIndex = 3;
-            ValuesLabel.Text = "label1";
+            EnumsListBox.FormattingEnabled = true;
+            EnumsListBox.ItemHeight = 20;
+            EnumsListBox.Location = new Point(47, 76);
+            EnumsListBox.Name = "EnumsListBox";
+            EnumsListBox.Size = new Size(150, 104);
+            EnumsListBox.TabIndex = 0;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(791, 450);
+            ClientSize = new Size(720, 395);
             Controls.Add(tabControl1);
             Name = "MainForm";
             Text = "Form1";
@@ -135,5 +155,7 @@
         private Label ValuesLabel;
         private Label EnumLabel;
         private ListBox ValuesListBox;
+        private Label ValueLabel;
+        private TextBox ValuesTextBox;
     }
 }
