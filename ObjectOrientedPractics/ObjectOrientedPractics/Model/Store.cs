6ecdc10efd1335cd3f629dba14aa299943a2ef26
Kirill Aksenov <kiraaxenow@gmail.com>
2024-09-27 14:29:52 +0700

    Создал класс Store

---
+++ ObjectOrientedPractics/ObjectOrientedPractics/Model/Store.cs
@@ -0,0 +1,41 @@
+﻿/// <summary>
+/// Хранит данные всех товаров и покупателей.
+/// </summary>
+public class Store
+{ 
+    /// <summary>
+    /// Товары.
+    /// </summary>
+    private List<Item> _items;
+
+    /// <summary>
+    /// Покупатели.
+    /// </summary>
+    private List<Customer> _customeers;
+
+    /// <summary>
+    /// Возвращает и задает список товаров.
+    /// </summary>
+    public List<Item> Items
+    {
+        get { return _items; }
+        set { _items = value; }
+    }
+
+    /// <summary>
+    /// Возвращает и задает список покупателей.
+    /// </summary>
+    public List<Customer> Customers
+    { 
+        get { return _customeers; }
+        set { _customeers = value; }
+    }
+
+    public Store()
+    {
+        Items = new List<Item>();
+        Customers = new List<Customer>();
+    }
+}
+
+
