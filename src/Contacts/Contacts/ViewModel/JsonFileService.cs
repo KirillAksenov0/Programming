e7d0125544d3f3dd59eb6fd84361005f4b2fadbf
Kirill Aksenov <kiraaxenow@gmail.com>
2025-03-19 17:26:06 +0700

    Исправил замечания по второй лабораторной работе

--- src/Contacts/Contacts/ViewModel/JsonFileService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Runtime.Serialization.Formatters;
-using System.Runtime.Serialization.Json;
-
-public class JsonFileService 
-{
-
-}
